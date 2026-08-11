01137d6e9b60208606b6e1f19d2d57a981ae5717
Ahmad Raihan <81221022+raihannn22@users.noreply.github.com>
2026-08-11 23:03:46 +0700

    first

---
+++ plugins/foo-header-check-policy/src/main/java/my/gravitee/extension/policy/FooHeaderCheckPolicy.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package my.gravitee.extension.policy;
+
+import io.gravitee.common.http.HttpStatusCode;
+import io.gravitee.gateway.api.Request;
+import io.gravitee.gateway.api.Response;
+import io.gravitee.policy.api.PolicyChain;
+import io.gravitee.policy.api.PolicyResult;
+import io.gravitee.policy.api.annotations.OnRequest;
+import io.gravitee.policy.api.annotations.OnResponse;
+
+@SuppressWarnings("unused")
+public class FooHeaderCheckPolicy {
+
+    /**
+     * The associated configuration to this FooHeaderCheck Policy
+     */
+    private FooHeaderCheckPolicyConfiguration configuration;
+
+    /**
+     * Create a new FooHeaderCheck Policy instance based on its associated configuration
+     *
+     * @param configuration the associated configuration to the new FooHeaderCheck Policy instance
+     */
+    public FooHeaderCheckPolicy(FooHeaderCheckPolicyConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    @OnRequest
+    public void onRequest(Request request, Response response, PolicyChain policyChain) {
+        // Add a dummy header
+        request.headers().set("X-DummyHeader", configuration.getStringParam());
+
+        // Finally continue chaining
+        policyChain.doNext(request, response);
+    }
+
+    @OnResponse
+    public void onResponse(Request request, Response response, PolicyChain policyChain) {
+        if (isASuccessfulResponse(response)) {
+            policyChain.doNext(request, response);
+        } else {
+            policyChain.failWith(
+                PolicyResult.failure(HttpStatusCode.INTERNAL_SERVER_ERROR_500, "Not a successful response :-("));
+        }
+    }
+
+    private static boolean isASuccessfulResponse(Response response) {
+        switch (response.status() / 100) {
+            case 1:
+            case 2:
+            case 3:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+}
