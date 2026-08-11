def7ee7859637efabd5363f363657d197899ad23
Ahmad Raihan <81221022+raihannn22@users.noreply.github.com>
2026-08-12 00:48:35 +0700

    second

---
+++ plugins/testing-policy/src/main/java/my/gravitee/extension/policy/FooHeaderCheckPolicy.java
@@ -0,0 +1,131 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package my.gravitee.extension.policy;
+
+import io.gravitee.common.http.HttpStatusCode;
+import io.gravitee.gateway.api.Request;
+import io.gravitee.gateway.api.Response;
+import io.gravitee.gateway.api.buffer.Buffer;
+import io.gravitee.gateway.api.stream.BufferedReadWriteStream;
+import io.gravitee.gateway.api.stream.ReadWriteStream;
+import io.gravitee.gateway.api.stream.SimpleReadWriteStream;
+import io.gravitee.policy.api.PolicyChain;
+import io.gravitee.policy.api.PolicyResult;
+import io.gravitee.policy.api.annotations.OnRequestContent;
+import io.gravitee.policy.api.annotations.OnResponse;
+import com.google.protobuf.util.JsonFormat;
+import my.gravitee.extension.policy.proto.HelloRequest;
+
+@SuppressWarnings("unused")
+public class FooHeaderCheckPolicy {
+
+    /**
+     * The associated configuration to this FooHeaderCheck Policy
+     */
+    private final FooHeaderCheckPolicyConfiguration configuration;
+
+    /**
+     * Create a new FooHeaderCheck Policy instance based on its associated configuration
+     *
+     * @param configuration the associated configuration to the new FooHeaderCheck Policy instance
+     */
+    public FooHeaderCheckPolicy(FooHeaderCheckPolicyConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    @OnRequestContent
+    public ReadWriteStream<Buffer> onRequestContent(Request request, PolicyChain policyChain) {
+        return new BufferedReadWriteStream() {
+            private final Buffer buffer = Buffer.buffer();
+
+            @Override
+            public SimpleReadWriteStream<Buffer> write(Buffer content) {
+                buffer.appendBuffer(content);
+                return this;
+            }
+
+            @Override
+            public void end() {
+                try {
+                    String jsonStr = buffer.toString();
+
+                    // Parse JSON to HelloRequest Protobuf message
+                    HelloRequest.Builder builder = HelloRequest.newBuilder();
+                    JsonFormat.parser().ignoringUnknownFields().merge(jsonStr, builder);
+                    HelloRequest protobufMessage = builder.build();
+                    byte[] protobufBytes = protobufMessage.toByteArray();
+
+                    byte[] finalBytes;
+                    String contentType;
+
+                    if (configuration.isUseGrpcFraming()) {
+                        // Standard gRPC framing: 1-byte compression flag + 4-byte message length
+                        int messageLength = protobufBytes.length;
+                        finalBytes = new byte[5 + messageLength];
+                        finalBytes[0] = 0; // uncompressed
+                        finalBytes[1] = (byte) ((messageLength >> 24) & 0xFF);
+                        finalBytes[2] = (byte) ((messageLength >> 16) & 0xFF);
+                        finalBytes[3] = (byte) ((messageLength >> 8) & 0xFF);
+                        finalBytes[4] = (byte) (messageLength & 0xFF);
+                        System.arraycopy(protobufBytes, 0, finalBytes, 5, messageLength);
+                        contentType = "application/grpc";
+                    } else {
+                        finalBytes = protobufBytes;
+                        contentType = "application/x-protobuf";
+                    }
+
+                    Buffer newBuffer = Buffer.buffer(finalBytes);
+
+                    // Update HTTP Request Headers
+                    request.headers().set("Content-Type", contentType);
+                    request.headers().set("Content-Length", Integer.toString(newBuffer.length()));
+
+                    // Write the transformed buffer downstream
+                    super.write(newBuffer);
+                    super.end();
+
+                } catch (Exception e) {
+                    policyChain.failWith(PolicyResult.failure(
+                            HttpStatusCode.BAD_REQUEST_400,
+                            "Failed to convert JSON request payload to Protobuf: " + e.getMessage()
+                    ));
+                }
+            }
+        };
+    }
+
+    @OnResponse
+    public void onResponse(Request request, Response response, PolicyChain policyChain) {
+        if (isASuccessfulResponse(response)) {
+            policyChain.doNext(request, response);
+        } else {
+            policyChain.failWith(
+                PolicyResult.failure(HttpStatusCode.INTERNAL_SERVER_ERROR_500, "Not a successful response :-("));
+        }
+    }
+
+    private static boolean isASuccessfulResponse(Response response) {
+        switch (response.status() / 100) {
+            case 1:
+            case 2:
+            case 3:
+                return true;
+            default:
+                return false;
+        }
+    }
+}
+
---
+++ plugins/testing-policy/src/test/java/my/gravitee/extension/policy/FooHeaderCheckPolicyIntegrationTest.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package my.gravitee.extension.policy;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import my.gravitee.extension.policy.proto.HelloRequest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import my.gravitee.extension.policy.FooHeaderCheckPolicy;
+import my.gravitee.extension.policy.FooHeaderCheckPolicyConfiguration;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import java.util.Map;
+import io.gravitee.apim.gateway.tests.sdk.AbstractPolicyTest;
+import io.gravitee.apim.gateway.tests.sdk.annotations.DeployApi;
+import io.gravitee.apim.gateway.tests.sdk.annotations.GatewayTest;
+import io.gravitee.apim.gateway.tests.sdk.configuration.GatewayConfigurationBuilder;
+import io.gravitee.apim.gateway.tests.sdk.policy.fakes.Header1Policy;
+import io.gravitee.apim.gateway.tests.sdk.policy.PolicyBuilder;
+import io.gravitee.definition.model.Api;
+import io.gravitee.plugin.policy.PolicyPlugin;
+import io.reactiverse.junit5.web.WebClientOptionsInject;
+import io.reactivex.observers.TestObserver;
+import io.vertx.reactivex.core.buffer.Buffer;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.reactivex.ext.web.client.HttpResponse;
+import io.vertx.reactivex.ext.web.client.WebClient;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+@GatewayTest
+@DeployApi("/my/gravitee/extension/policy/apis/FooHeaderCheck.json")
+public class FooHeaderCheckPolicyIntegrationTest extends AbstractPolicyTest<FooHeaderCheckPolicy, FooHeaderCheckPolicyConfiguration> {
+
+    // You can override configuration of the WebClient with the following lines.
+    // @WebClientOptionsInject
+    // public WebClientOptions options = new WebClientOptions().setDefaultHost("localhost").setDefaultPort(gatewayPort());
+
+    @Override
+    public void configureApi(Api api) {
+        // Here you can override each api to deploy programmatically.
+    }
+
+    @Override
+    public void configureWireMock(WireMockConfiguration configuration) {
+        // Here you can override the wiremock configuration. Wiremock is the component mocking your backends.
+    }
+
+    @Override
+    public void configureGateway(GatewayConfigurationBuilder gatewayConfigurationBuilder) {
+        // Here you can override the gateway configuration if needed.
+    }
+
+    @Override
+    public void configurePolicies(Map<String, PolicyPlugin> policies) {
+        // Here you can register custom testing policies, for example:
+        policies.put("header-policy1", PolicyBuilder.build("header-policy1", Header1Policy.class));
+
+        // you have the same capabilities with:
+        // - configureConnectors(Map<String, ConnectorPlugin> connectors)
+        // - configureResources(Map<String, ResourcePlugin> resources)
+        // - configureReporters(Map<String, Reporter> reporters)
+    }
+
+    @Test
+    @DisplayName("Should test the policy deployed on an in memory gateway")
+    public void shouldTestFooHeaderCheckPolicyDeployedOnAnInMemoryGateway(WebClient client) throws Exception {
+        wiremock.stubFor(post("/backend").willReturn(ok("response from backend")));
+
+        io.vertx.core.json.JsonObject jsonBody = new io.vertx.core.json.JsonObject();
+        jsonBody.put("greeting", "Hello From Gravitee!");
+
+        final TestObserver<HttpResponse<Buffer>> obs = client.post("/test")
+            .rxSendJsonObject(jsonBody)
+            .test();
+
+        awaitTerminalEvent(obs);
+        obs
+            .assertComplete()
+            .assertValue(response -> {
+                assertThat(response.statusCode()).isEqualTo(200);
+                assertThat(response.bodyAsString()).isEqualTo("response from backend");
+                return true;
+            })
+        .assertNoErrors();
+
+        var requests = wiremock.findAll(postRequestedFor(urlPathEqualTo("/backend")));
+        assertThat(requests).hasSize(1);
+        byte[] receivedBody = requests.get(0).getBody();
+        HelloRequest requestMessage = HelloRequest.parseFrom(receivedBody);
+        assertThat(requestMessage.getGreeting()).isEqualTo("Hello From Gravitee!");
+    }
+}
